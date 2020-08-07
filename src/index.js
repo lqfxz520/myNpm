7041397885933911f24d0f26fb3130eaf7d53e95
linqingfeng <459024711@qq.com>
2020-08-07 16:27:10 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const reg = /(Y{1,4}|M{1,2}|D{1,2}|d{1,2}|H{1,2}|h{1,2}|m{1,2}|s{1,2}|S{1,3})/g
+
+function formateTime(timeStamp, template = 'YYYY-MM-DD') {
+    const dateObj = new Date(timeStamp)
+    const translation = template.replace(reg, function (match) {
+        // console.log(match)
+        let temp = String(translateCase(match, dateObj))
+        // 位数解决， 情况一：要求长度少于实际长度, 情况二：要求长度大于实际长度
+        const len = match.length - temp.length
+        if (len > 0) {
+            let str = ''
+            for (let i = 0; i < len; i++) str += '0'
+            temp = str + temp
+        } else if (len < 0 && match.includes('Y')) {
+            temp = temp.slice(len)
+        }
+        return temp
+    })
+
+    return translation
+}
+
+function translateCase(s, dateObj) {
+    let timeValue = ''
+    switch (s) {
+        case 'Y':
+        case 'YY':
+        case 'YYY':
+        case 'YYYY':
+            timeValue = dateObj.getFullYear()
+            break
+        case 'M':
+        case 'MM':
+            timeValue = dateObj.getMonth() + 1
+            break
+        case 'D':
+        case 'DD':
+            timeValue = dateObj.getDate()
+            break
+        case 'd':
+        case 'dd':
+            timeValue = dateObj.getDay()
+            break
+        case 'H':
+        case 'HH':
+            timeValue = dateObj.getHours()
+            break
+        case 'h':
+        case 'hh':
+            const hours = dateObj.getHours()
+            timeValue = hours > 12 ? hours - 12 : hours
+            break
+        case 'm':
+        case 'mm':
+            timeValue = dateObj.getMinutes()
+            break
+        case 'S':
+        case 'SS':
+        case 'SSS':
+            timeValue = dateObj.getMilliseconds()
+            break
+        case 's':
+        case 'ss':
+            timeValue = dateObj.getSeconds()
+            break
+        default:
+            timeValue = s
+    }
+    return timeValue
+}
+
+console.log(formateTime(new Date(), 'M/D/YY HH'))
