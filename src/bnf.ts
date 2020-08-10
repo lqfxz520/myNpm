3e574ce9674e21af47ec7a64cc6a737d72dba4fd
linqingfeng <459024711@qq.com>
2020-08-10 19:04:27 +0800

    理解编译原理

---
+++ src/bnf.ts
@@ -0,0 +1,42 @@
+let token = []
+const start = char => {
+    if([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(char)) {
+        token.push(char)
+        return inNumber
+    }
+    if(['+', '-', '*', '/']) {
+        emmitToken(char, char)
+        return start
+    }
+    if(char === ' ') {
+        return start
+    }
+    if(char === '\r' || char=== '\n') {
+        return start
+    }
+}
+
+const inNumber = char => {
+    if([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(char)) {
+        token.push(char)
+        return inNumber
+    } else {
+        emmitToken("Number", token.join(''))
+        token = []
+        return start(char)
+    }
+}
+
+function emmitToken(type, value) {
+    console.log(value)
+}
+
+const input = '1024 + 2 * 256'
+
+let state = start
+
+for(let c of input.split('')) {
+    state = state(c)
+}
+
+state(Symbol('EOF'))
